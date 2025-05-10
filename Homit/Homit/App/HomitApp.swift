5bca26a7eea1a3e7c183d693a3756d0b3b6816b5
Minsoo Kang <alstn38@naver.com>
2025-05-10 15:20:18 +0900

    init: Homit project 생성

---
+++ Homit/Homit/App/HomitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomitApp.swift
+//  Homit
+//
+//  Created by 강민수 on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct HomitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
