dd4c6619ecfc81e6a71c52c8547d80e2bae3a5b5
Minsoo Kang <alstn38@naver.com>
2025-05-11 23:32:40 +0900

    feat: UserLoginDTO Model 정의

---
+++ Homit/Homit/Data/Network/DTO/User/UserLoginDTO.swift
@@ -0,0 +1,26 @@
+//
+//  UserLoginDTO.swift
+//  Homit
+//
+//  Created by 강민수 on 5/11/25.
+//
+
+import Foundation
+
+struct UserLoginDTO: Decodable {
+    let userId: String
+    let email: String
+    let nickName: String
+    let profileImage: String?
+    let accessToken: String
+    let refreshToken: String
+    
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case email
+        case nickName = "nick"
+        case profileImage
+        case accessToken
+        case refreshToken
+    }
+}
