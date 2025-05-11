857557e32c34e56c39a5c31250450ffefee2ddf5
Minsoo Kang <alstn38@naver.com>
2025-05-11 14:25:28 +0900

    setting: Project Font 설정
    - Pretendard 폰트 적용
    - 영덕 해파리 폰트 적용

---
+++ Homit/Homit/Resource/Extension/Font+.swift
@@ -0,0 +1,23 @@
+//
+//  Font+.swift
+//  Homit
+//
+//  Created by 강민수 on 5/11/25.
+//
+
+import SwiftUI
+
+enum PretendardFont {
+    static let title1 = Font.custom("Pretendard-Bold", size: 20)
+    static let body1 = Font.custom("Pretendard-Medium", size: 16)
+    static let body2 = Font.custom("Pretendard-Medium", size: 14)
+    static let body3 = Font.custom("Pretendard-Medium", size: 13)
+    static let caption1 = Font.custom("Pretendard-Regular", size: 12)
+    static let caption2 = Font.custom("Pretendard-Regular", size: 10)
+    static let caption3 = Font.custom("Pretendard-Regular", size: 8)
+}
+
+enum HaeparangFont {
+    static let title1 = Font.custom("Yeongdeok-Haeparang", size: 20)
+    static let caption1 = Font.custom("Yeongdeok-Haeparang", size: 12)
+}
