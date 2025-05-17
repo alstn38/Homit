5f53f139bda350199535507f88b794e6e7000da8
Minsoo Kang <alstn38@naver.com>
2025-05-17 18:39:14 +0900

    feat: AuthRepositoryKey Dependency 추가

---
+++ Homit/Homit/Dependency/Repository/Auth/AuthRepositoryKey.swift
@@ -0,0 +1,25 @@
+//
+//  AuthRepositoryKey.swift
+//  Homit
+//
+//  Created by 강민수 on 5/17/25.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+private enum AuthRepositoryKey: DependencyKey {
+    static var liveValue: AuthRepository {
+        DefaultAuthRepository(
+            keychainTokenStorage: DependencyValues.live.keychainTokenStorage
+        )
+    }
+}
+
+extension DependencyValues {
+    var authRepository: AuthRepository {
+        get { self[AuthRepositoryKey.self] }
+        set { self[AuthRepositoryKey.self] = newValue }
+    }
+}
