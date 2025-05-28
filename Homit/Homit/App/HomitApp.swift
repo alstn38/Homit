24e9344c2b8717bff925fee5e252b316eb4a681a
Minsoo Kang <alstn38@naver.com>
2025-05-29 00:33:23 +0900

    feat: HomitApp kakao url 처리 기능 추가

--- Homit/Homit/App/HomitApp.swift
+++ Homit/Homit/App/HomitApp.swift
@@ -8,16 +8,27 @@
 import SwiftUI
 
 import ComposableArchitecture
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct HomitApp: App {
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
+    init() {
+        KakaoSDK.initSDK(appKey: Secret.kakaoAppKey)
+    }
+    
     var body: some Scene {
         WindowGroup {
             RootView(store: Store(initialState: AppFeature.State()) {
                 AppFeature()
+            })
+            .onOpenURL(perform: { url in
+                if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                    let _ = AuthController.handleOpenUrl(url: url)
+                }
             })
         }
     }
