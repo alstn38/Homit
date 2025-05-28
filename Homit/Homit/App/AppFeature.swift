0249224e5089cf77b0a7f59931f4c2e59a4c28b8
Minsoo Kang <alstn38@naver.com>
2025-05-28 14:51:24 +0900

    feat: AppFeature 화면 분기 기능 구현

---
+++ Homit/Homit/App/AppFeature.swift
@@ -0,0 +1,63 @@
+//
+//  AppFeature.swift
+//  Homit
+//
+//  Created by 강민수 on 5/19/25.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+struct AppFeature {
+    
+    @ObservableState
+    struct State {
+        var splash = SplashFeature.State()
+        var login: LoginFeature.State?
+        var tabBar: TabBarFeature.State?
+    }
+    
+    enum Action {
+        case splash(SplashFeature.Action)
+        case login(LoginFeature.Action)
+        case tabBar(TabBarFeature.Action)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .splash(.proceedToNextScreen):
+                switch state.splash.authState {
+                case .authenticated:
+                    state.tabBar = TabBarFeature.State()
+                    state.login = nil
+                    
+                case .unauthenticated:
+                    state.login = LoginFeature.State()
+                    state.tabBar = nil
+                }
+                
+                return .none
+                
+            case .login(.path(.element(id: _, action: .emailLogin(.loginSuccess)))):
+                state.tabBar = TabBarFeature.State()
+                state.login = nil
+                return .none
+                
+            case .splash, .login, .tabBar:
+                return .none
+            }
+        }
+        Scope(state: \.splash, action: \.splash) {
+            SplashFeature()
+        }
+        .ifLet(\.login, action: \.login) {
+            LoginFeature()
+        }
+        .ifLet(\.tabBar, action: \.tabBar) {
+            TabBarFeature()
+        }
+    }
+}
