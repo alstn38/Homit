40f53e8d9fcf949d99b1f3800adaf8da1fa546b2
Minsoo Kang <alstn38@naver.com>
2025-05-28 14:47:16 +0900

    feat: AuthRepository loginWithEmail 인터페이스 추가

--- Homit/Homit/Domain/Repository/Auth/AuthRepository.swift
+++ Homit/Homit/Domain/Repository/Auth/AuthRepository.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 protocol AuthRepository {
+    /// 로그인을 한 적이 있는지 확인하는 메서드
+    func isAuthenticated() -> Bool
+    
+    /// 이메일을 통한 회원가입 메서드
     func signUpWithEmail(email: String, password: String, nickName: String) async throws
+    
+    /// 이메일을 통한 로그인 메서드
+    func loginWithEmail(email: String, password: String) async throws
 }
