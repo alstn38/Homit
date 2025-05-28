eaf23fd254ae057f93a14d40228e131e0faf569f
Minsoo Kang <alstn38@naver.com>
2025-05-28 14:49:40 +0900

    feat: RootView 화면 분기 View 추가

---
+++ Homit/Homit/App/RootView.swift
@@ -0,0 +1,28 @@
+//
+//  RootView.swift
+//  Homit
+//
+//  Created by 강민수 on 5/19/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+
+struct RootView: View {
+    
+    @Perception.Bindable
+    var store: StoreOf<AppFeature>
+    
+    var body: some View {
+        WithPerceptionTracking {
+            if let tabBarStore = store.scope(state: \.tabBar, action: \.tabBar) {
+                TabBarView(store: tabBarStore)
+            } else if let loginStore = store.scope(state: \.login, action: \.login) {
+                LoginView(store: loginStore)
+            } else {
+                SplashView(store: store.scope(state: \.splash, action: \.splash))
+            }
+        }
+    }
+}
