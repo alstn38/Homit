77cfe163c27f189503db554c944f2e42c97b1242
Minsoo Kang <alstn38@naver.com>
2025-05-28 16:15:07 +0900

    feat: LoginFeature Apple Login 기능 추가

--- Homit/Homit/Present/Login/Login/LoginFeature.swift
+++ Homit/Homit/Present/Login/Login/LoginFeature.swift
@@ -5,22 +5,38 @@
 //  Created by 강민수 on 5/12/25.
 //
 
+import AuthenticationServices
 import ComposableArchitecture
 
 @Reducer
 struct LoginFeature {
     
+    @Dependency(\.appleLoginUseCase) var appleLoginUseCase
+    
     @ObservableState
     struct State {
         var path = StackState<Path.State>()
         var isLoading: Bool = false
+        
+        @Presents
+        var alert: AlertState<Action.Alert>?
     }
     
     enum Action {
         case path(StackActionOf<Path>)
-        case appleLoginDidTap
+        case appleLogin(idToken: String, nickName: String)
         case kakaoLoginDidTap
         case emailLoginDidTap
+        
+        case appleLoginSuccess
+        case appleLoginFailure(String)
+        
+        case alert(PresentationAction<Alert>)
+        
+        @CasePathable
+        enum Alert: Equatable {
+            case errorAlertDismissed
+        }
     }
     
     var body: some ReducerOf<Self> {
@@ -33,8 +49,20 @@ struct LoginFeature {
             case .path:
                 return .none
                 
-            case .appleLoginDidTap:
-                return .none
+            case .appleLogin(let idToken, let nickName):
+                state.isLoading = true
+                
+                return .run { send in
+                    do {
+                        try await appleLoginUseCase.execute(
+                            idToken: idToken,
+                            nickName: nickName
+                        )
+                        await send(.appleLoginSuccess)
+                    } catch {
+                        await send(.appleLoginFailure(error.localizedDescription))
+                    }
+                }
                 
             case .kakaoLoginDidTap:
                 return .none
@@ -42,9 +70,37 @@ struct LoginFeature {
             case .emailLoginDidTap:
                 state.path.append(.emailLogin(EmailLoginFeature.State()))
                 return .none
+                
+            case .appleLoginSuccess:
+                state.isLoading = false
+                return .none
+                
+            case .appleLoginFailure(let errorMessage):
+                state.isLoading = false
+                state.alert = AlertState(
+                    title: {
+                        TextState("애플 로그인 실패")
+                    },
+                    actions: {
+                        ButtonState(role: .cancel, action: .errorAlertDismissed) {
+                            TextState("확인")
+                        }
+                    },
+                    message: {
+                        TextState(errorMessage)
+                    }
+                )
+                return .none
+                
+            case .alert(.presented(.errorAlertDismissed)):
+                return .none
+                
+            case .alert(.dismiss):
+                return .none
             }
         }
         .forEach(\.path, action: \.path)
+        .ifLet(\.$alert, action: \.alert)
     }
     
     @Reducer
