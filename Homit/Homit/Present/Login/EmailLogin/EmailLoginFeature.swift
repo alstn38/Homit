ebd365c03a891d7794b3a2ff7fea74e62fb8ea39
Minsoo Kang <alstn38@naver.com>
2025-05-28 14:46:32 +0900

    feat: EmailLoginFeature 로그인 기능 추가

--- Homit/Homit/Present/Login/EmailLogin/EmailLoginFeature.swift
+++ Homit/Homit/Present/Login/EmailLogin/EmailLoginFeature.swift
@@ -13,11 +13,16 @@ import ComposableArchitecture
 struct EmailLoginFeature {
     
     @Dependency(\.dismiss) var dismiss
+    @Dependency(\.authRepository) var authRepository
     
     @ObservableState
     struct State {
         var email: String = ""
         var password: String = ""
+        var isLoading: Bool = false
+        
+        @Presents
+        var alert: AlertState<Action.Alert>?
         
         var isValidButton: Bool {
             !email.isEmpty && !password.isEmpty
@@ -28,6 +33,15 @@ struct EmailLoginFeature {
         case binding(BindingAction<State>)
         case loginButtonDidTap
         case signUpButtonDidTap
+        
+        case loginSuccess
+        case loginFailure(String)
+        case alert(PresentationAction<Alert>)
+        
+        @CasePathable
+        enum Alert: Equatable {
+            case errorAlertDismissed
+        }
     }
     
     var body: some ReducerOf<Self> {
@@ -38,11 +52,48 @@ struct EmailLoginFeature {
                 return .none
                 
             case .loginButtonDidTap:
-                return .none
+                state.isLoading = true
+                
+                return .run { [email = state.email, password = state.password] send in
+                    do {
+                        try await authRepository.loginWithEmail(email: email, password: password)
+                        await send(.loginSuccess)
+                    } catch {
+                        await send(.loginFailure(error.localizedDescription))
+                    }
+                }
                 
             case .signUpButtonDidTap:
                 return .none
+                
+            case .loginSuccess:
+                state.isLoading = false
+                return .none
+                
+            case .loginFailure(let errorMessage):
+                state.isLoading = false
+                state.alert = AlertState(
+                    title: {
+                        TextState("로그인 실패")
+                    },
+                    actions: {
+                        ButtonState(role: .cancel, action: .errorAlertDismissed) {
+                            TextState("확인")
+                        }
+                    },
+                    message: {
+                        TextState(errorMessage)
+                    }
+                )
+                return .none
+                
+            case .alert(.presented(.errorAlertDismissed)):
+                return .none
+                
+            case .alert(.dismiss):
+                return .none
             }
         }
+        .ifLet(\.$alert, action: \.alert)
     }
 }
