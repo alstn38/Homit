2c6b55cf246ed7e9de244bf56797202c5f92598d
Minsoo Kang <alstn38@naver.com>
2025-05-31 18:47:05 +0900

    refactor: NetworkService Interceptor Session 추가

--- Homit/Homit/Data/Network/Common/NetworkService.swift
+++ Homit/Homit/Data/Network/Common/NetworkService.swift
@@ -13,13 +13,46 @@ final class NetworkService {
     
     static let shared = NetworkService()
     
-    private init() { }
+    private let session: Session
+    private let sessionWithoutInterceptor: Session
+    
+    private init() {
+        let tokenStorage = DefaultKeychainTokenStorage()
+        let interceptor = TokenInterceptor(tokenStorage: tokenStorage)
+        self.session = Session(interceptor: interceptor)
+        self.sessionWithoutInterceptor = Session()
+    }
     
     func request<T: Router, U: Decodable>(
         router: T,
         responseType: U.Type
     ) async throws -> U {
-        let dataTask = AF.request(router)
+        let dataTask = session.request(router)
+            .validate(statusCode: 200..<300)
+            .serializingData()
+
+        let response = await dataTask.response
+
+        switch response.result {
+        case .success(let data):
+            do {
+                let decoded = try JSONDecoder().decode(U.self, from: data)
+                return decoded
+            } catch {
+                throw NetworkError.decodeError
+            }
+
+        case .failure:
+            let statusCode = response.response?.statusCode
+            throw router.throwError(data: response.data, statusCode: statusCode)
+        }
+    }
+    
+    func requestWithoutInterceptor<T: Router, U: Decodable>(
+        router: T,
+        responseType: U.Type
+    ) async throws -> U {
+        let dataTask = sessionWithoutInterceptor.request(router)
             .validate(statusCode: 200..<300)
             .serializingData()
 
