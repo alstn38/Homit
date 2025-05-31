b44572d1c395d84a33cb68f8336c2fc56010a8e3
Minsoo Kang <alstn38@naver.com>
2025-05-31 18:46:08 +0900

    chore: NetworkError Token Error 추가

--- Homit/Homit/Data/Network/Common/NetworkError.swift
+++ Homit/Homit/Data/Network/Common/NetworkError.swift
@@ -14,6 +14,8 @@ enum NetworkError: Error {
     case badRequest(message: String?) // 400
     case unauthorized(message: String?) // 401
     case conflict(message: String?) // 409
+    case refreshTokenExpired(message: String?) // 418 - RefreshToken 만료
+    case accessTokenExpired(message: String?) // 419 - AccessToken 만료
     case unknown
 }
 
@@ -33,6 +35,10 @@ extension NetworkError: LocalizedError {
             return message ?? "인증에 실패했습니다. (401)"
         case .conflict(let message):
             return message ?? "이미 존재하는 항목입니다. (409)"
+        case .refreshTokenExpired(let message):
+            return message ?? "RefreshToken이 만료되었습니다. (418)"
+        case .accessTokenExpired(let message):
+            return message ?? "AccessToken이 만료되었습니다. (419)"
         case .unknown:
             return "알 수 없는 오류가 발생했습니다."
         }
