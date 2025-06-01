e9e80474c0ce91bc6d9435fcb3f12d795710cbc4
Minsoo Kang <alstn38@naver.com>
2025-06-01 15:08:58 +0900

    feat: AdvertisementEntity 정의

---
+++ Homit/Homit/Domain/Entity/AdvertisementEntity.swift
@@ -0,0 +1,13 @@
+//
+//  AdvertisementEntity.swift
+//  Homit
+//
+//  Created by 강민수 on 5/31/25.
+//
+
+import Foundation
+
+struct AdvertisementEntity {
+    let title: String      /// 광고 제목
+    let content: String    /// 광고 내용
+}
