f34dfceac968cbb0b22a878696dbd522fc90bbb6
Minsoo Kang <alstn38@naver.com>
2025-06-01 15:17:22 +0900

    feat: EstateRepositoryKey Dependency Key 추가

---
+++ Homit/Homit/Dependency/Repository/Estate/EstateRepositoryKey.swift
@@ -0,0 +1,25 @@
+//
+//  EstateRepositoryKey.swift
+//  Homit
+//
+//  Created by 강민수 on 5/31/25.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+private enum EstateRepositoryKey: DependencyKey {
+    static var liveValue: EstateRepository {
+        DefaultEstateRepository(
+            geocoderService: DependencyValues.live.geocoderService
+        )
+    }
+}
+
+extension DependencyValues {
+    var estateRepository: EstateRepository {
+        get { self[EstateRepositoryKey.self] }
+        set { self[EstateRepositoryKey.self] = newValue }
+    }
+}
